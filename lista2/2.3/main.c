775e337c294959906a67119faf3466e552baa184
Yuri <yuri_dsx_@hotmail.com>
2021-10-08 20:59:46 -0300

    feat: take out both accounts

--- lista2/2.3/main.c
+++ lista2/2.3/main.c
@@ -35,22 +35,46 @@ int main(int argc, char const *argv[])
 				depositaConta(&saving_account, valueOperation);
 				showCurrentValues(&cc_account, &saving_account);
 				break;
-
+			case 3:
+				printf("Vc escolheu retirada em conta corrente, por favor, insira o valor:\n");
+				scanf("%f", &valueOperation);
+				if(obtemSaldo(&cc_account) < valueOperation){
+					if(obtemSaldo(&saving_account) >= valueOperation){
+						showCurrentValues(&cc_account, &saving_account);
+						retiraDaConta(&saving_account, valueOperation);
+						showCurrentValues(&cc_account, &saving_account);
+						depositaConta(&cc_account, valueOperation);
+						showCurrentValues(&cc_account, &saving_account);
+						retiraDaConta(&cc_account, valueOperation);												
+					} else{
+						printf("Saldo insuficiente:\n");
+					}
+				} else{
+					retiraDaConta(&cc_account, valueOperation);	
+				}
+				showCurrentValues(&cc_account, &saving_account);
+				break;
+			case 4:
+				showCurrentValues(&cc_account, &saving_account);
+				printf("Vc escolheu retirada em conta poupança, por favor, insira o valor:\n");
+				scanf("%f", &valueOperation);
+				if(obtemSaldo(&saving_account) >= valueOperation){
+					retiraDaConta(&saving_account, valueOperation);				
+				} else{
+					printf("Saldo insuficiente:\n");
+				}
+				showCurrentValues(&cc_account, &saving_account);
+				break;
 		}
 		printf("\nEscolha uma opcao:\n");
 		printf(" 1.Deposito conta corrente\n 2.Deposito Poupanca\n 3.Retirada conta corrente\n 4.Retirada poupanca\n 5.Fim\n\n");
 		scanf("%d", &op);
-
-	}
-			
-	
-
-	
+	}	
 	return 0;
 }
 
 
 void showCurrentValues(Conta *cc_acc, Conta *sav_acc){
-	printf("Conta corrente - $ %.2f\n", obtemSaldo(cc_acc));
+	printf("Conta corrente - $ %.2f\n\n", obtemSaldo(cc_acc));
 	printf("Conta poupanca - $ %.2f\n", obtemSaldo(sav_acc));
 }
